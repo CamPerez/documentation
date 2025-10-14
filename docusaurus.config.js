bf29f3181da9d15f2c402fc23211caeb533a9ae0
Camila Pérez <camperezarevalo@hotmail.com>
2025-10-14 14:49:18 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
     experimental_faster: true,
   },
   // Set the production url of your site here
-  url: 'https://docs.travelgate.com',
+  url: 'https://camperez.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/documentation/',
